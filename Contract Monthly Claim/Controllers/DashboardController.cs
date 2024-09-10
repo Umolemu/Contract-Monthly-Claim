2d91b8b72f419816ede11341b335187434b94fea
Umolemu <lambvries@gmail.com>
2024-09-10 12:25:21 +0200

    Added and styled sidebar

---
+++ Contract Monthly Claim/Controllers/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Contract_Monthly_Claim.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
